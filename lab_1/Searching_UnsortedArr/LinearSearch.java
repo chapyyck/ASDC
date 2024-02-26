f0033a424e6113881675f9b89f90831492c1bd94
chapyyck <mr.skvirtl@gmail.com>
2024-02-26 19:38:11 +0200

    Added First Searching in Unsorted arrays

---
+++ lab_1/Searching_UnsortedArr/LinearSearch.java
@@ -0,0 +1,25 @@
+package Searching_UnsortedArr;
+
+public class LinearSearch {
+    public static SearchResult linearSearch(int[] array, int target) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == target) {
+                return new SearchResult(true, i);
+            }
+        }
+        return new SearchResult(false, -1);
+    }
+
+    public static void main(String[] args) {
+        int[] array = {5, 2, 9, 1, 7, 4, 8};
+        int target = 7;
+        SearchResult result = linearSearch(array, target);
+
+        if (result.found) {
+            System.out.println("Элемент " + target + " найден в массиве по индексу " + result.index);
+        }
+        else {
+            System.out.println("Элемент " + target + " не найден в массиве");
+        }
+    }
+}
---
+++ lab_1/Searching_UnsortedArr/SearchResult.java
@@ -0,0 +1,10 @@
+package Searching_UnsortedArr;
+
+class SearchResult {
+    boolean found;
+    int index;
+    public SearchResult(boolean found, int index) {
+        this.found = found;
+        this.index = index;
+    }
+}
